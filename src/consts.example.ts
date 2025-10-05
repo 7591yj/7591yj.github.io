107b045baf8ecfd807195a6b043c9516b8ab950d
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-10-05 10:55:42 +0900

    chore: change to use consts, add example

---
+++ src/consts.example.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+
+export const SITE_TITLE = '';
+export const SITE_DESCRIPTION = '';
+
+export const GH_LINK = '';
+export const EMAIL = '';
+export const NAME = '';
