23ee11216fbf20a1c9c733321785ac4a030ac1d1
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-10-05 14:53:53 +0900

    feat(personal-projects): update collection, add layout

---
+++ src/content.config.ts
@@ -0,0 +1,55 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+
+const blog = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
+});
+
+const personalProjects = defineCollection({
+  // Load Markdown and MDX files in the `src/content/personal-projects/` directory.
+  loader: glob({
+    base: "./src/content/personal-projects",
+    pattern: "**/*.{md,mdx}",
+  }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
+});
+
+const softwareDevelopment = defineCollection({
+  // Load Markdown and MDX files in the `src/content/software-development/` directory.
+  loader: glob({
+    base: "./src/content/software-development",
+    pattern: "**/*.{md,mdx}",
+  }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      // Transform string to Date object
+      pubDate: z.coerce.date(),
+      updatedDate: z.coerce.date().optional(),
+      heroImage: image().optional(),
+    }),
+});
+
+export const collections = { softwareDevelopment, personalProjects, blog };
