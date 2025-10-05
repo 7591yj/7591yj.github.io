8f43528f5192d3eca0b58b6120d66d8740247b77
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-10-05 14:45:01 +0900

    chore: integrate mdx and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+// @ts-check
+
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { PROD_URL } from "./src/consts.js";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: PROD_URL,
+  integrations: [mdx(), sitemap()],
+});
