b4eb27c16b6186fc8b5aafc63a745d663525acca
Yeongjin Kim <77034308+7591yj@users.noreply.github.com>
2025-10-05 14:46:09 +0900

    chore: reformat

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # CodeFolio
+
 An open-source portfolio for devs to showcase their projects, coding skills and perosnal story.
 
 ## License
+
 This project is licensed under the MIT License (see [LICENSE](./LICENSE)). It incorporates Codefolio theme licensed under MIT (see [docs/LICENSE-Codefolio](./docs/LICENSE-Codefolio)).
--- src/consts.example.ts
+++ src/consts.example.ts
@@ -1,8 +1,9 @@
 // Place any global data in this file.
 
-export const SITE_TITLE = '';
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = "";
+export const SITE_DESCRIPTION = "";
 
-export const GH_LINK = '';
-export const EMAIL = '';
-export const NAME = '';
+export const GH_LINK = "";
+export const EMAIL = "";
+export const NAME = "";
+export const PROFILE_IMAGE = "";
