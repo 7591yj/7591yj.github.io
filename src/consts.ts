bdd4baa6b6b5706fe3a6aab41b9c0f74d949619b
7591yj <77034308+7591yj@users.noreply.github.com>
2026-02-26 17:27:51 +0900

    refactor: globalize constants

---
+++ src/consts.ts
@@ -0,0 +1,8 @@
+export const SITE_NAME = "7591yj";
+export const AUTHOR = "Yeongjin Kim";
+export const EMAIL = "yeongjin.kim@proton.me";
+
+export const SOCIAL = {
+  github: "https://github.com/7591yj",
+  linkedin: "https://www.linkedin.com/in/yeongjin-kim-58b2ab351",
+};
