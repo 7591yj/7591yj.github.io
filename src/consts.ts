e4a8ce962075591c38033fb16d31f950222cb75e
7591yj <77034308+7591yj@users.noreply.github.com>
2026-05-04 18:23:38 +0900

    chore: update consts

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,10 @@ export const SITE_DESCRIPTION =
   "Personal portfolio and blog of 7591yj - software development, design, and research.";
 export const SITE_OG_IMAGE = "/og-default.png";
 export const AUTHOR = "Yeongjin Kim";
-export const EMAIL = "yeongjin.kim@proton.me";
+export const EMAIL = "admin@7591yj.com";
 
 export const SOCIAL = {
   github: "https://github.com/7591yj",
-  linkedin: "https://www.linkedin.com/in/yeongjin-kim-58b2ab351",
+  codeberg: "https://codeberg.org/7591yj",
+  linkedin: "https://www.linkedin.com/in/yeongjink",
 };
