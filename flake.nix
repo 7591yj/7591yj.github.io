0986b7b5f8dfc2297de35f1ffd897f2e9f65fa1f
7591yj <77034308+7591yj@users.noreply.github.com>
2026-05-16 14:58:56 +0900

    chore: replace devenv with nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "7591yj.github.io dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            pnpm
+          ];
+
+          shellHook = ''
+            echo "Node: $(node --version)"
+            echo "pnpm: $(pnpm --version)"
+          '';
+        };
+      }
+    );
+}
